d9bf14eb7c772543dab73b13be4c427ceff76759
Syed-Urwah <syedurwahdev@gmail.com>
2023-01-17 19:34:12 +0500

    connecting to atlas

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/Blog"
+// const mongoURI = "mongodb://localhost:27017/Blog"
+const mongoURI = "mongodb+srv://syedurwah:fKL4YSfchTqrYF8T@cluster1.z8bwmqy.mongodb.net/?retryWrites=true&w=majority"
+// const mongoURI = process.env.MONGO_STRING
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
