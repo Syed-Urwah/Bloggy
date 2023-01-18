a02aea25871cae8a087da1e0ab9430529a8782f5
Syed-Urwah <syedurwahdev@gmail.com>
2023-01-18 11:27:18 +0500

    Enviromenet Update

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
+
 
 // const mongoURI = "mongodb://localhost:27017/Blog"
-const mongoURI = "mongodb+srv://syedurwah:fKL4YSfchTqrYF8T@cluster1.z8bwmqy.mongodb.net/?retryWrites=true&w=majority"
-// const mongoURI = process.env.MONGO_STRING
+// const mongoURI = "mongodb+srv://syedurwah:fKL4YSfchTqrYF8T@cluster1.z8bwmqy.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = process.env.MONGO_STRING
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
